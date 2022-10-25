fd91a3f9852173f3de575dff6daf8a3502fa5150
Farhan Ahmed <azzassfa@gmail.com>
2022-10-24 18:37:39 -0700

    Consolidated all onClick functions into one

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -6,40 +6,31 @@ import Button from '@mui/material/Button';
 function Counter() {
     const [counter, setCounter] = useState(0);
 
-    function incCounter() {
-        const c=1;
+    function updateCounter(e) {
+        let c=5;
+
+        if (e.target.name==="incCounter") c=1; 
+        else if (e.target.name==="decCounter") c=-1;
+        else if (e.target.name==="incCounterRandom") c=Math.floor(Math.random()*100);
+        else if (e.target.name==="decCounterRandom") c=-1*Math.floor(Math.random()*100);
+        
         let newCounter = counter+c;
         setCounter(newCounter);
-    }
-
-    function decCounter() {
-        const c=1;
-        let newCounter = counter-c;
-        setCounter(newCounter);
-    }
-
-    function incCounterRandom() {
-        const c=Math.floor(Math.random()*100);
-        let newCounter = counter+c;
-        setCounter(newCounter);
-    }
-
-    function decCounterRandom() {
-        const c=Math.floor(Math.random()*100);
-        let newCounter = counter-c;
-        setCounter(newCounter);
+        return(
+            <div>test</div>
+        );
     }
 
     return (
         <div>
             <h1 style={{marginTop:"0"}}>{counter}</h1>
             <p>
-            <Button variant="contained" onClick={incCounter} style={{margin:"10px", backgroundColor:"green"}}>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;+ 1&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</Button>
-            <Button variant="contained" onClick={decCounter} style={{margin:"10px", backgroundColor:"red"}}>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;- 1&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</Button>
+            <Button variant="contained" name="incCounter" onClick={updateCounter} style={{margin:"10px", backgroundColor:"green"}}>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;+ 1&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</Button>
+            <Button variant="contained" name="decCounter" onClick={updateCounter} style={{margin:"10px", backgroundColor:"red"}}>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;- 1&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</Button>
             </p>
             <p>
-            <Button variant="contained" onClick={incCounterRandom} style={{margin:"10px", backgroundColor:"green"}}>+ Random</Button>
-            <Button variant="contained" onClick={decCounterRandom} style={{margin:"10px", backgroundColor:"red"}}>- Random</Button>
+            <Button variant="contained" name="incCounterRandom" onClick={updateCounter} style={{margin:"10px", backgroundColor:"green"}}>+ Random</Button>
+            <Button variant="contained" name="decCounterRandom" onClick={updateCounter} style={{margin:"10px", backgroundColor:"red"}}>- Random</Button>
             </p>
         </div>
     );
